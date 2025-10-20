ae4fbc21e23d38fc2d28ce67270b4d022f7d81fe
Sergio <you@example.com>
2025-10-20 19:50:02 +0200

    actu

---
+++ Miguel/01_print.py
@@ -0,0 +1,108 @@
+# estaEsUnaVariable = camel case
+# esta_es_una_variable = snake case
+# esta-es-una-variable = kebab case
+
+# print("hola mundo otra vez")
+
+# if True:
+#     print("ooooooooo")
+
+# variable = "tre"
+# print(variable)
+
+# nombre="Sergio"
+# edad=24
+# print("Mi nombre es :"+nombre+" y tengo "+str(edad)+" años.")
+
+# print(f"El año que viene tendre {edad+1} años")
+
+# nombre= "sergio Guerrero"
+# print(nombre.title())
+# print(nombre.upper())
+# print(nombre.lower())
+
+# print("Python")
+# print("\tPython")
+# print("Lenguaje \nPython"
+
+# var="             Lenguaje      Python           "
+# print(len(var.lstrip()))
+
+# var="eeeeeeeeeeeeeeeeeeeeeeeeeeeaaaaaaaaaaaaaaaaaaaaaaaaa"
+# var_striped=var.strip("a")
+# print(var_striped)
+
+# url="http://suarezdefigueroa.es:8080/?redirect=0"
+# print(url.removeprefix("http://"))
+# print(url.removesuffix("/?redirect=0"))
+
+# Pide la edad del usuario y muestra un mensaje segun su rango de edad
+# -menor de 12= eres un niño
+# -entre 12 y 17 eres un adolescente
+# -entre 18 y 64 eres un adulto
+# -mayor de 65 eres un jubilado
+
+# edad=int(input('introduzca su edad\n'))
+# if edad<12:
+#     print("eres un niño")
+# elif edad>=12 and edad <=17:
+#     print("eres un adolescente")
+# elif edad>=18 and edad <=64:
+#     print("eres un adulto")
+# elif edad >=65:
+#     print("eres un jubilado")
+# else:
+#     print("dato incorrecto introducido")
+
+
+# for i in range(5):
+#     print(i)
+
+# que pida un numero y muestre su tabla de multiplicar del 1 al 10
+
+# num=int(input("introduzca el numero que quieras ver la tabla de multiplicar\n"))
+# for i in range(1,11):
+#     print(f"{num} X {i}:{num*i}")
+
+# coches=["Mclaren","Ferrari","Toyota"]
+
+# for coche in coches:
+#     print(coche)
+
+
+# cont=1
+# while cont<=5:
+#     print("a")
+#     cont+=1
+
+
+# """
+# Elige un numero secreto entre 1 y 10
+# el usuario debe adivinarlo y el programa dira si ha acertado
+# """
+
+# import random
+
+# num_secreto = random.randint(1,10)
+# numero=0
+# while numero != num_secreto:
+#     numero=int(input("introduce el numero secreto: "))
+#     if numero>num_secreto:
+#         print("te has pasao")
+#     elif numero<num_secreto:
+#         print("te has quedao corto")
+
+# print("has ganado")
+
+"""
+Pide una palabra al usuario y muestra cuantas letras tiene pero sin contar las vocales
+"""
+
+palabra=input("introduzca una palabra : ")
+cont=0
+for letra in palabra:
+    if letra in "aeiouAEIOU":
+        continue
+    cont+=1
+    print(letra,end=" ")
+print(f"\ncontiene {cont} letras sin contar vocales")
