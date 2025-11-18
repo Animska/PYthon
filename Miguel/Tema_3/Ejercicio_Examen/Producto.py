d66ec5f25e699c88a547e31b473c92ebfdde3642
Sergio <you@example.com>
2025-11-18 20:59:02 +0100

    correcion ejercicio_examen

--- Miguel/Tema_3/Ejercicio_Examen/Producto.py
+++ Miguel/Tema_3/Ejercicio_Examen/Producto.py
@@ -3,6 +3,8 @@ class Producto:
     def __init__(self,nombre,codigo_lote):
         self.__nombre=nombre
         self.codigo_lote=codigo_lote
+        Producto.STOCK_TOTAL[self.nombre] = self.STOCK_TOTAL.get(self.nombre, 0) + 1
+        print(f"Producto {self.nombre} (Lote:{self.codigo_lote}) creado y añadido al stock")
 
     @property
     def nombre(self):
@@ -17,17 +19,24 @@ class Producto:
     @codigo_lote.setter
     def codigo_lote(self, valor):
     #Setter: valida antes de asignar el valor.
-        if len(valor)==8 and valor[0]=='L':
-            self.__codigo_lote = valor
-            self.STOCK_TOTAL[self.nombre] = self.STOCK_TOTAL.get(self.nombre, 0) + 1
-            print(f"Producto {self.__nombre} (Lote:{self.codigo_lote}) creado y añadido al stock")
-        else:
-            raise ValueError
+        if len(valor)!=8:
+            raise ValueError("ERROR LOTE! El codigo de lote no tiene la longitud adecuada")
+        if not valor.upper().startswith('L'):
+            raise ValueError("ERROR LOTE! El codigo de lote no empieza por L")
+        self.__codigo_lote = valor.upper()
+
         
     def vender(self):
-        if self.STOCK_TOTAL[self.nombre]>0:
-            self.STOCK_TOTAL[self.nombre]-=1
-            print(f"Venta de {self.nombre} realizada. Stock restante: {self.STOCK_TOTAL[self.nombre]}")
+        if self.STOCK_TOTAL[self.nombre]<=0:
+            raise RuntimeError(f"ERROR! No hay stock de {self.nombre}")
+        
+        self.STOCK_TOTAL[self.nombre]-=1
+        print(f"Venta de {self.nombre} realizada. Stock restante: {self.STOCK_TOTAL[self.nombre]}")
+
+    def __eq__(self, otro):
+        if isinstance(otro, Producto):
+            return self.__codigo_lote == otro.__codigo_lote
+        return False
         
 # producto1 = Producto('Rubicrono','LOTE-P45')
 # producto2 = Producto('Rubicrono','LOTE-P46')
--- Miguel/Tema_3/Ejercicio_Examen/Trazabilidad.py
+++ Miguel/Tema_3/Ejercicio_Examen/Trazabilidad.py
@@ -2,19 +2,26 @@ class Trazabilidad:
 
     @staticmethod
     def consultar_stock_total(stock_total_dict):
+        if not stock_total_dict:
+            print("El stock esta vacio")
+            return
+        
         for producto in sorted(stock_total_dict.keys()):
             print(f"Producto: {producto}, Stock disponible: {stock_total_dict[producto]}")
 
     @staticmethod
     def generar_reporte_bajo_stock(stock_total_dict,umbral):
-        if all(stock > umbral for stock in stock_total_dict.values()):
-            print("“Todos los productos están por encima del umbral”")
-        else:
-            for producto in sorted(stock_total_dict.keys()):
-                print(f"Producto: {producto}, Stock disponible: {stock_total_dict[producto]}")
+        productos_bajo_stock = {nombre:cantidad for nombre,cantidad in stock_total_dict.items() if cantidad <= umbral}
+        print("REPORTE DE PRODUCTOS BAJO STOCK")
+        if not productos_bajo_stock:
+            print("El stock de todos los productos esta por encma del umbral")
+            return
+        for nombre,cantidad in productos_bajo_stock.items():
+            print(f"{nombre} cantidad:{cantidad}")
 
     @staticmethod
     def  listar_nombres_disponibles(stock_total_dict):
-        inventario = ", ".join(producto for producto in stock_total_dict.keys())
+        nombres_stock = [nombre for nombre,cantidad in stock_total_dict.items() if cantidad > 0]
+        inventario = ", ".join(sorted(nombres_stock))
         return inventario
 
